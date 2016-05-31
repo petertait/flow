944e9e3543f9a5aa67c7b19e37dfd23fb803d2e3
Peter Tait <contact@petertait.com>
2016-05-31 10:09:47 +0100

    add images to stream

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,31 @@
 dropbox.authenticate( { client_id: 'qbryowo5frs09ht' }, function(){
   dropbox('files/list_folder', {path: ''}, function(result) {
-     console.log(result.entries);
-     console.log(result.entries.path_lower);
+
+    var stream = document.querySelector(".stream");
+    var results = result.entries;
+    var resultsLength = results.length;
+
+    for (var i = 0; i < resultsLength; i++) {
+      console.log(results[i]);
+
+      resultPreview = document.createElement('img');
+      resultPreview.classList.add('preview');
+      // resultPreview.setAttribute("src", file/path_lower);
+
+      stream.appendChild(resultPreview);
+    }
+
+    var list = function(obj) {
+      for(var prop in obj) {
+        console.log(prop);
+      }
+    };
+
+    list(path_lower);
+
   });
+
+
 
   // dropbox('files/get_preview', {path: ''}, function(path) {
   //    console.log(path);
