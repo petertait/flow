562b0b9718cee7be3c4cc5321924dfff59ed8215
Peter Tait <contact@petertait.com>
2016-06-27 21:36:14 +0100

    it works

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-dropbox.authenticate( { client_id: 'qbryowo5frs09ht', redirect_uri: 'http://localhost:7000/' }, function(){
+dropbox.authenticate( { client_id: 'qbryowo5frs09ht', redirect_uri: 'http://localhost:7000' }, function(){
   dropbox('files/list_folder', {path: ''}, function(result) {
 
     var stream = document.querySelector(".stream");
@@ -11,24 +11,19 @@ dropbox.authenticate( { client_id: 'qbryowo5frs09ht', redirect_uri: 'http://loca
         // onComplete: function(result){ console.log(result); }
 
         onComplete: function(result, response, r ){
-          console.log(result);console.log(response);console.log(r);
-          var blob = new Blob(response.response, {type: ’application/octet-stream’});
+          // console.log(result);
+          console.log(response);
+          // console.log(r);
+
+          var urlCreator = window.URL || window.webkitURL;
+          var imageUrl = urlCreator.createObjectURL(response);
           resultPreview = document.createElement('img');
           resultPreview.classList.add('preview');
-          resultPreview.src = window.URL.createObjectURL(blob);
+          resultPreview.src = imageUrl;
+          document.getElementById('stream').appendChild(resultPreview);
 
         }
-
-
-
       });
-
-      // dropbox('files/get_thumbnail', {path: result.path_lower}, function(result) {
-      //   resultPreview = document.createElement('img');
-      //   resultPreview.classList.add('preview');
-      //   resultPreview.setAttribute("src", result.path_lower);
-      //   stream.appendChild(resultPreview);
-      // });
     }
   });
 });
